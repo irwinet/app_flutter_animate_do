4c954dbfcbf6873a256161473789be793bd87a59
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-08-30 22:13:17 -0500

    Add animate_do fontawesome

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app_animate_do/src/pages/pagina1_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animate Do',
+      debugShowCheckedModeBanner: false,
+      home: Pagina1Page(),
+    );
+  }
+}
---
+++ lib/src/pages/pagina1_page.dart
@@ -0,0 +1,71 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class Pagina1Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: FadeIn(
+          duration: Duration(milliseconds: 500),
+          child: Text('Animate_do')
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: FaIcon(FontAwesomeIcons.twitter)
+          ),
+
+          SlideInLeft(
+            from: 100,
+            child: IconButton(
+              onPressed: (){
+                Navigator.push(context, CupertinoPageRoute(builder: (BuildContext builder) => Pagina1Page()));
+              },
+              icon: Icon(Icons.navigate_next)
+            ),
+          )
+        ],
+      ),
+
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          onPressed: (){},
+          child: FaIcon(FontAwesomeIcons.play),
+        ),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElasticIn(
+              delay: Duration(milliseconds: 1100),
+              child: Icon(Icons.new_releases, color: Colors.blue, size: 40,)
+            ),
+            FadeInDown(
+              delay: Duration(milliseconds: 200),
+              child: Text('Titulo', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),)
+            ),
+            FadeInDown(
+              delay: Duration(milliseconds: 800),
+              child: Text('Soy un texto pequeño', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),)
+            ),
+            FadeInLeft(
+              delay: Duration(milliseconds: 1100),
+              child: Container(
+                width: 220,
+                height: 2,
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+   );
+  }
+}
